dc5d3d222043194157916c6b485c756a622c3e4e
JinCheng <jincheng@haplox.com>
2018-01-26 18:19:45 +0800

    更新：可以查看多层文件夹

--- index.js
+++ index.js
@@ -8,56 +8,60 @@ var file_stats = []
 function readDir() {
     fs.readdir(dirpath, function(err, files) {
         console.log('')
+
         if(!files.length) {
             return console.log('\033[31m 这是一个空的文件夹！ \033[39m \n')
         }
+
         console.log(' 当前位置 \033[33m' + dirpath + ': \n \033[39m')
+
         // 清空当前目录下所有文件stat信息
         file_stats = []
+
         // 列出文件
-        forFiles(dirpath, files, 0)
-    })
-}
+        forFiles(0)
 
-function forFiles(dirpath, files, i) {
-    var filename = files[i]
-    fs.stat(`${dirpath}/${filename}`, function(err, stat) {
-        file_stats.push(stat)
-        if(stat.isDirectory()) {
-            console.log('  ' + i + ' \033[36m' + filename + '/ \033[39m')
-        } else {
-            console.log('  ' + i + ' \033[90m' + filename + '\033[39m')
-        }
-        i++
-        if(i > files.length-1) {
-            console.log('')
-            stdout.write('\033[33m 等待输入：\033[39m')
-            stdin.resume()
-            stdin.setEncoding('utf8')
-            stdin.on('data', function(data) {
-                readFile(data, files, file_stats)
+        function forFiles(i) {
+            var filename = files[i]
+            fs.stat(`${dirpath}/${filename}`, function(err, stat) {
+                file_stats.push(stat)
+                if(stat.isDirectory()) {
+                    console.log('  ' + i + ' \033[36m' + filename + '/ \033[39m')
+                } else {
+                    console.log('  ' + i + ' \033[90m' + filename + '\033[39m')
+                }
+                i++
+                if(i > files.length-1) {
+                    console.log('')
+                    stdout.write('\033[33m 等待输入：\033[39m')
+                    stdin.resume()
+                    stdin.setEncoding('utf8')
+                    // 监听输入，读取相应文件
+                    stdin.on('data', readFile)
+                } else {
+                    forFiles(i)
+                }
             })
-        } else {
-            forFiles(dirpath, files, i)
+        }
+
+        function readFile(data) {
+            var filename = files[Number(data)]
+            var stat = file_stats[Number(data)]
+            if(!filename) {
+                stdout.write('\033[31m 等待输入：\033[39m')
+            } else {
+                if(stat.isDirectory()) {
+                    dirpath = `${dirpath}\\${filename}`
+                    stdin.removeListener('data', readFile)
+                    readDir()
+                } else {
+                    // fs.readFile(`${dirpath}/${filename}`, 'utf8', function(err, data) {
+                    //     console.log(data)
+                    // })
+                }
+            }
         }
     })
-}
-
-function readFile(data, files, file_stats) {
-    var filename = files[Number(data)]
-    var stat = file_stats[Number(data)]
-    if(!filename) {
-        stdout.write('\033[31m 等待输入：\033[39m')
-    } else {
-        if(stat.isDirectory()) {
-            dirpath = `${dirpath}\\${filename}`
-            readDir()
-        } else {
-            // fs.readFile(`${dirpath}/${filename}`, 'utf8', function(err, data) {
-            //     console.log(data)
-            // })
-        }
-    }
 }
 
 readDir()
